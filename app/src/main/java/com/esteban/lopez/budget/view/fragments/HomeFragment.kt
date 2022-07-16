eb2373298a5fabd718780ad8ad4bea0da6601ad1
Esteban <ingeniero.master19@interrapidisimo.com>
2022-07-15 19:31:34 -0500

    Changes

--- app/src/main/java/com/esteban/lopez/budget/view/fragments/HomeFragment.kt
+++ app/src/main/java/com/esteban/lopez/budget/view/fragments/HomeFragment.kt
@@ -1,37 +1,16 @@
 package com.esteban.lopez.budget.view.fragments
 
-import android.R.*
-import android.content.DialogInterface
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
-import androidx.appcompat.app.AlertDialog
-import androidx.core.text.isDigitsOnly
-import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
-import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
-import com.esteban.lopez.budget.Application
 import com.esteban.lopez.budget.R
 import com.esteban.lopez.budget.databinding.FragmentHomeBinding
-import com.esteban.lopez.budget.databinding.NewValueDialogBinding
-import com.esteban.lopez.budget.model.db.entities.Category
-import com.esteban.lopez.budget.model.db.entities.Expense
-import com.esteban.lopez.budget.model.db.entities.Income
-import com.esteban.lopez.budget.view.Utils
-import com.esteban.lopez.budget.view.Utils.Companion.removeCommas
 import com.esteban.lopez.budget.view.fragments.dialogs.NewValueDialogFragment
-import com.esteban.lopez.budget.viewmodel.*
-import com.google.android.material.snackbar.Snackbar
-import kotlinx.coroutines.launch
-
 
 class HomeFragment : Fragment() {
 
